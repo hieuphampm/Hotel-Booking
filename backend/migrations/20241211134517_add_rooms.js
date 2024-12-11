db68d8a02df0136b180c8df19ce1c6148a31b762
Phuoc Hieu <hieuphamminh976@gmail.com>
2024-12-11 21:12:15 +0700

    update

---
+++ backend/migrations/20241211134517_add_rooms.js
@@ -0,0 +1,22 @@
+exports.up = async function(knex) {
+    await knex.raw(`
+        CREATE TABLE room(  
+            room_id int NOT NULL PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+            name text,
+            price int,
+            imageUrl text, 
+            description text  
+        );
+        COMMENT ON TABLE room IS 'Room';
+        COMMENT ON COLUMN room.name IS 'Room Name';
+        COMMENT ON COLUMN room.price IS 'Room Price';
+        COMMENT ON COLUMN room.imageUrl IS 'Room Images';
+        COMMENT ON COLUMN room.description IS 'Room Description';
+    `);
+  };
+  
+  exports.down = async function(knex) {
+    await knex.raw(`
+        DROP TABLE room;
+    `);
+  };
